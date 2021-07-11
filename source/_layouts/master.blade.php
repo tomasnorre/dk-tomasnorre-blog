5fafc6c697fd21254f772e698000875e8eb5b7db
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2021-07-11 12:08:41 +0200

    Change footer/copyright

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -50,7 +50,7 @@
         <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
             <ul class="flex flex-col md:flex-row justify-center list-none">
                 <li class="md:mr-2">
-                    &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
+                    &copy; <a href="{{ $page->baseUrl }}" title="Tomas Norre">Tomas Norre</a> {{ date('Y') }}.
                 </li>
 
                 <li>
