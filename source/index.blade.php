88100b27b14c4d053f6820dc99e837d59260c5ff
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2025-07-14 20:05:39 +0200

    fix: downgrade h1 to h2 to respect only 1 h1

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,7 +8,7 @@
             <div class="p-6">
                 <p class="text-sm text-gray-500 mb-1">{{ $featuredPost->getDate()->format('F j, Y') }}</p>
                 <a href="{{ $featuredPost->getUrl() }}">
-                    <h1 class="text-3xl font-bold mb-2">{{ $featuredPost->title }}</h1>
+                    <h2 class="text-3xl font-bold mb-2">{{ $featuredPost->title }}</h2>
                     <p class="mb-4 text-gray-600">{!! $featuredPost->getExcerpt() !!}</p>
                 </a>
                 <a href="{{ $featuredPost->getUrl() }}" class="text-blue-600 font-semibold hover:underline">Read More →</a>
