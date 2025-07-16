813635bff74d58383803ceb1b280baf049eefa8c
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2025-07-16 17:34:20 +0200

    fix: change how highlightjs includes languages

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -12,19 +12,33 @@ import hljs from 'highlight.js/lib/core';
 import 'highlight.js/styles/default.css';
 import 'boxicons';
 
-hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
-hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
-hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'));
-hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
-hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
-hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'));
-hljs.registerLanguage('php', require('highlight.js/lib/languages/php'));
-hljs.registerLanguage('scss', require('highlight.js/lib/languages/scss'));
-hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'));
-hljs.registerLanguage('diff', require('highlight.js/lib/languages/diff'));
-hljs.registerLanguage('apacheconf', require('highlight.js/lib/languages/apache'));
-hljs.registerLanguage('txt', require('highlight.js/lib/languages/plaintext'));
+import bash from 'highlight.js/lib/languages/bash';
+import css from 'highlight.js/lib/languages/css';
+import xml from 'highlight.js/lib/languages/xml';
+import javascript from 'highlight.js/lib/languages/javascript';
+import json from 'highlight.js/lib/languages/json';
+import markdown from 'highlight.js/lib/languages/markdown';
+import php from 'highlight.js/lib/languages/php';
+import scss from 'highlight.js/lib/languages/scss';
+import yaml from 'highlight.js/lib/languages/yaml';
+import diff from 'highlight.js/lib/languages/diff';
+import apache from 'highlight.js/lib/languages/apache';
+import plaintext from 'highlight.js/lib/languages/plaintext';
+
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('html', xml); // HTML = XML in highlight.js
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('json', json);
+hljs.registerLanguage('markdown', markdown);
+hljs.registerLanguage('php', php);
+hljs.registerLanguage('scss', scss);
+hljs.registerLanguage('yaml', yaml);
+hljs.registerLanguage('diff', diff);
+hljs.registerLanguage('apacheconf', apache);
+hljs.registerLanguage('txt', plaintext);
 
 document.querySelectorAll('pre code').forEach((block) => {
     hljs.highlightElement(block); // Changed from highlightBlock to highlightElement in newer versions of highlight.js
 });
+ 
