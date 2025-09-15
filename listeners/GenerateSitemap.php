cb513780ae7b1f941d2e080ef2c3d854172d9453
Tomas Norre Mikkelsen <tomasnorre@gmail.com>
2025-09-15 10:40:41 +0200

    feat: set update frequency and modified time according to filechanges

--- listeners/GenerateSitemap.php
+++ listeners/GenerateSitemap.php
@@ -25,15 +25,17 @@ class GenerateSitemap
         }
 
         $sitemap = new Sitemap($jigsaw->getDestinationPath() . '/sitemap.xml');
-
+        $sourcePath = $jigsaw->getSourcePath();
         collect($jigsaw->getOutputPaths())
             ->reject(function ($path) {
                 return $this->isExcluded($path);
-            })->each(function ($path) use ($baseUrl, $sitemap) {
+            })->each(function ($path) use ($baseUrl, $sitemap, $sourcePath ) {
                 if(!str_contains($path, 'feed.atom')) {
                     $path .= '/';
                 }
-                $sitemap->addItem(rtrim($baseUrl, '/') . $path, time(), Sitemap::DAILY);
+                $time = $this->getModifiedTime($path, $sourcePath);
+                $changeFrequency = $this->isMainOrCategoryPage($path) ? Sitemap::DAILY : Sitemap::MONTHLY;
+                $sitemap->addItem(rtrim($baseUrl, '/') . $path, $time, $changeFrequency) ;
         });
 
         $sitemap->write();
@@ -43,4 +45,66 @@ class GenerateSitemap
     {
         return Str::is($this->exclude, $path);
     }
+
+    private function getModifiedTime(string $path, string $sourcePath): int
+    {
+        date_default_timezone_set('Europe/Copenhagen');
+        if($this->isMainOrCategoryPage($path)) {
+            return time();
+        }
+
+        $file = $this->getFilenameAndPath($path, $sourcePath);
+        $lastModified = false;
+        if (file_exists($file)) {
+            $lastModified = filemtime($file);
+        }
+
+        if (is_int($lastModified)) {
+            return $lastModified;
+        }
+
+        return 0;
+    }
+
+    private function isMainOrCategoryPage(string $path): bool
+    {
+        if ($this->isCategoryIndex($path)) {
+            return true;
+        }
+
+        if(in_array($path,  ['/blog/', '/about/', '/privacy/'])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function isCategoryIndex(string $path): bool
+    {
+        // Path is categories index
+        if(str_contains($path, '/categories/')) {
+            return true;
+        }
+
+        // Path is categories index number based.
+        if (preg_match('#^/blog(/([0-9]+)/)?$#', $path)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $path
+     * @param string $sourcePath
+     * @return string
+     */
+    private function getFilenameAndPath(string $path, string $sourcePath): string
+    {
+        $filename = basename(trim($path, '/')) . '.md';
+        $sourceDirectory = $sourcePath . '/_posts/';
+
+        return $sourceDirectory . $filename;
+    }
+
 }
