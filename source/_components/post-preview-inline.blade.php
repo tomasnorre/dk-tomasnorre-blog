c88c468513600317005acaeccea48f97ebf3da99
Tomas Norre Mikkelsen <mail@7th-green.com>
2026-02-04 22:25:42 +0100

    playing around

--- source/_components/post-preview-inline.blade.php
+++ source/_components/post-preview-inline.blade.php
@@ -1,21 +1,19 @@
-<div class="bg-base rounded-2xl shadow-md overflow-hidden flex flex-col border-base-content border">
-    <img src="{{ $post->cover_image }}" alt="{{ $post->cover_alt }}" class="w-full h-48 object-cover" />
-
-    <!-- Make the inner content grow with flex -->
-    <div class="p-6 flex flex-col flex-1">
-        <p class="text-sm text-base-content mb-1">
+<div class="card bg-base-100 w-full shadow-sm">
+    <figure>
+        <img
+                src="{{ $post->cover_image }}"
+                alt="{{ $post->cover_alt }}" />
+    </figure>
+    <div class="card-body">
+        <p class="text-sm text-base-content">
             {{ $post->getDate()->format('F j, Y') }}
         </p>
-
-        <a href="{{ $post->getUrl() }}" title="Read more - {{ $post->title }}">
-            <h2 class="text-xl font-bold mb-2">{{ $post->title }}</h2>
-            <p class="mb-4 text-base-content ">{!! $post->getExcerpt(200) !!}</p>
-        </a>
-
-        <!-- Spacer + Read More -->
-        <div class="mt-auto pt-4">
-            <a href="{{ $post->getUrl() }}" title="Read more - {{ $post->title }}"
-               class="text-blue-600 font-semibold hover:underline">Read More →</a>
+        <h2 class="card-title text-xl font-bold">
+            {{ $post->title }}
+        </h2>
+        <p class="text-lg">{!! $post->getExcerpt(200) !!}</p>
+        <div class="card-actions justify-end">
+            <a href="{{ $post->getUrl() }}" class="btn btn-primary">Read More →</a>
         </div>
     </div>
 </div>
--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -1,16 +1,16 @@
 <nav id="menu" class="hidden lg:flex items-center justify-end text-lg">
     <a title="Blog" href="/"
-        class="ml-6 link link-primary {{ $page->isActive('/') ? 'active' : '' }}">
+        class="ml-6 link link-primary no-underline {{ $page->isActive('/') ? 'active' : '' }}">
         Blog
     </a>
 
     <a title="About" href="/about"
-        class="ml-6 link link-primary {{ $page->isActive('/about') ? 'active' : '' }}">
+        class="ml-6 link link-primary no-underline {{ $page->isActive('/about') ? 'active' : '' }}">
         About
     </a>
 
     <a title="Contact" href="/privacy"
-        class="ml-6 link link-primary {{ $page->isActive('/privacy') ? 'active' : '' }}">
+        class="ml-6 link link-primary no-underline {{ $page->isActive('/privacy') ? 'active' : '' }}">
         Privacy
     </a>
 </nav>
